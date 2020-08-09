9d5a0a87aea156a9d08cead1a8717c775b4d26a7
angelo128 <fabrizio.angelo.spitale@gmail.com>
2020-08-09 16:05:10 -0300

    Comentario de Jessica modificado, linea 17 LoginPrueba.cs

--- Login/Login/LoginPrueba.cs
+++ Login/Login/LoginPrueba.cs
@@ -14,7 +14,7 @@ namespace Login
        
     {   
         //Definicion e inicializacion de variables [Comentario modificado]
-        //nuevo comentario de prueba
+        //nuevo comentario de prueba [Agregando texto al comentario de prueba]
         String usuario = "admin";
         String contrasena = "4862";
         public LoginPrueba()
