7ad42f6ab249d12caeccb8f2eb60910e23e7cd02
angelo128 <fabrizio.angelo.spitale@gmail.com>
2020-08-09 15:23:02 -0300

    nuevo comentario

--- Login/Login/LoginPrueba.cs
+++ Login/Login/LoginPrueba.cs
@@ -14,6 +14,7 @@ namespace Login
        
     {   
         //Definicion e inicializacion de variables [Comentario modificado]
+        //new comentario
         String usuario = "admin";
         String contrasena = "4862";
         public LoginPrueba()
