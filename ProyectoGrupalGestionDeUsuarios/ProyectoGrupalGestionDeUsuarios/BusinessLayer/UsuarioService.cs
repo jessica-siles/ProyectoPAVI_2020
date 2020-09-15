f72d9c82be1315aaabb1fd90af2834956f27f2e3
Siles-Je <silesjessica@hotmail.com>
2020-09-15 16:31:32 -0300

    union de funcionalidad eliminar y modificar, y agregadod e recargo de grilla

---
+++ ProyectoGrupalGestionDeUsuarios/ProyectoGrupalGestionDeUsuarios/BusinessLayer/UsuarioService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProyectoGrupalGestionDeUsuarios.Entities;
+using ProyectoGrupalGestionDeUsuarios.DataAccessLayer;
+
+namespace ProyectoGrupalGestionDeUsuarios.BusinessLayer
+{
+    public class UsuarioService
+    {
+        private UsuarioDao oUsuarioDao;
+
+        public UsuarioService()
+        {
+            oUsuarioDao = new UsuarioDao();
+        }
+        public IList<Usuario> ObtenerTodos()
+        {
+            return oUsuarioDao.GetAll();
+        }
+
+        //public Usuario ValidarUsuario(string usuario, string password)
+        //{
+        //    var usr = oUsuarioDao.GetUser(usuario);
+
+        //    if (usr.Password != null && usr.Password.Equals(password))
+        //    {
+        //        return usr;
+        //    }
+
+        //    return null;
+        //}
+        public Usuario ValidarUsuario(string usuario, string password)
+        {
+            var usr = oUsuarioDao.GetUser(usuario);
+
+            if (usr != null)
+            {
+                if (usr.Password != null && usr.Password.Equals(password))
+                {
+                    return usr;
+                }
+            }
+
+            return null;
+        }
+    }
+}
---
+++ ProyectoGrupalGestionDeUsuarios/ProyectoGrupalGestionDeUsuarios/Entities/Perfil.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using ProyectoGrupalGestionDeUsuarios.Entities;
+using System.Windows.Forms;
+
+namespace ProyectoGrupalGestionDeUsuarios.Entities
+{
+    public class Perfil        
+    {
+        public int IdPerfil { get; set; }
+        public string Nombre { get; set; }
+        public string Estado { get; set; }
+        public string Descripcion { get; set; }
+
+        public int Borrado { get; set; }
+
+        public override string ToString()
+        {
+            return Nombre;
+        }
+
+        public void cambiarEstado()
+        {
+            if (Estado is "False")
+            {
+                Estado = "Activo";
+            }
+            else
+            {
+                Estado = "Inactivo";
+            }
+        }
+        public void mostrarEstado(bool m)
+        {
+            MessageBox.Show(Convert.ToString(m), "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+    }
+}
