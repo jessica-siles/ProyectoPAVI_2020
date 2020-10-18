634f503464fdd7453f2df69dd9656529dc5ad29b
angelo128 <fabrizio.angelo.spitale@gmail.com>
2020-10-17 22:24:21 -0300

    frmConsultarPermisos agregado

---
+++ ProyectoGrupalGestionDeUsuarios/ProyectoGrupalGestionDeUsuarios/GUILayer/AMBC Permisos/frmConsltarPermisos.designer.cs
@@ -0,0 +1,251 @@
+﻿namespace ProyectoGrupalGestionDeUsuarios.GUILayer.AMBC_Permisos
+{
+    partial class frmConsltarPermisos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmConsltarPermisos));
+            this.panelBotones = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.groupBoxConsulta = new System.Windows.Forms.GroupBox();
+            this.dgvConsultaUsuarios = new System.Windows.Forms.DataGridView();
+            this.numForm = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.descripcion = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lblEstado = new System.Windows.Forms.Label();
+            this.cboPerfil = new System.Windows.Forms.ComboBox();
+            this.panelBotones.SuspendLayout();
+            this.groupBoxConsulta.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvConsultaUsuarios)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelBotones
+            // 
+            this.panelBotones.BackColor = System.Drawing.Color.Transparent;
+            this.panelBotones.Controls.Add(this.button1);
+            this.panelBotones.Controls.Add(this.btnCancelar);
+            this.panelBotones.Controls.Add(this.btnModificar);
+            this.panelBotones.Location = new System.Drawing.Point(38, 491);
+            this.panelBotones.Name = "panelBotones";
+            this.panelBotones.Size = new System.Drawing.Size(740, 80);
+            this.panelBotones.TabIndex = 8;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkViolet;
+            this.button1.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(188, 18);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(143, 53);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Eliminar";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.BackColor = System.Drawing.Color.DarkViolet;
+            this.btnCancelar.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelar.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnCancelar.Location = new System.Drawing.Point(563, 18);
+            this.btnCancelar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(156, 53);
+            this.btnCancelar.TabIndex = 0;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = false;
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.BackColor = System.Drawing.Color.DarkViolet;
+            this.btnModificar.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnModificar.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnModificar.Location = new System.Drawing.Point(14, 18);
+            this.btnModificar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(148, 53);
+            this.btnModificar.TabIndex = 1;
+            this.btnModificar.Text = "Modificar";
+            this.btnModificar.UseVisualStyleBackColor = false;
+            // 
+            // groupBoxConsulta
+            // 
+            this.groupBoxConsulta.BackColor = System.Drawing.Color.Transparent;
+            this.groupBoxConsulta.Controls.Add(this.dgvConsultaUsuarios);
+            this.groupBoxConsulta.Font = new System.Drawing.Font("Segoe UI Symbol", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBoxConsulta.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.groupBoxConsulta.Location = new System.Drawing.Point(38, 117);
+            this.groupBoxConsulta.Name = "groupBoxConsulta";
+            this.groupBoxConsulta.Size = new System.Drawing.Size(736, 368);
+            this.groupBoxConsulta.TabIndex = 9;
+            this.groupBoxConsulta.TabStop = false;
+            this.groupBoxConsulta.Text = "Listado De Formularios a los que el perfil seleccionado tiene acceso";
+            // 
+            // dgvConsultaUsuarios
+            // 
+            this.dgvConsultaUsuarios.AllowUserToAddRows = false;
+            this.dgvConsultaUsuarios.AllowUserToDeleteRows = false;
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.Black;
+            this.dgvConsultaUsuarios.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgvConsultaUsuarios.BackgroundColor = System.Drawing.Color.MediumOrchid;
+            this.dgvConsultaUsuarios.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dgvConsultaUsuarios.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvConsultaUsuarios.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.numForm,
+            this.Nombre,
+            this.descripcion});
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Segoe UI Symbol", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.CornflowerBlue;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.Desktop;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvConsultaUsuarios.DefaultCellStyle = dataGridViewCellStyle3;
+            this.dgvConsultaUsuarios.GridColor = System.Drawing.Color.Thistle;
+            this.dgvConsultaUsuarios.Location = new System.Drawing.Point(16, 30);
+            this.dgvConsultaUsuarios.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.dgvConsultaUsuarios.MultiSelect = false;
+            this.dgvConsultaUsuarios.Name = "dgvConsultaUsuarios";
+            this.dgvConsultaUsuarios.ReadOnly = true;
+            this.dgvConsultaUsuarios.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle4.ForeColor = System.Drawing.Color.Black;
+            this.dgvConsultaUsuarios.RowsDefaultCellStyle = dataGridViewCellStyle4;
+            this.dgvConsultaUsuarios.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvConsultaUsuarios.Size = new System.Drawing.Size(703, 330);
+            this.dgvConsultaUsuarios.TabIndex = 0;
+            // 
+            // numForm
+            // 
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.Indigo;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.Color.WhiteSmoke;
+            this.numForm.DefaultCellStyle = dataGridViewCellStyle2;
+            this.numForm.HeaderText = "Nº";
+            this.numForm.Name = "numForm";
+            this.numForm.ReadOnly = true;
+            this.numForm.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.numForm.Width = 50;
+            // 
+            // Nombre
+            // 
+            this.Nombre.HeaderText = "Nombre";
+            this.Nombre.Name = "Nombre";
+            this.Nombre.ReadOnly = true;
+            this.Nombre.Width = 250;
+            // 
+            // descripcion
+            // 
+            this.descripcion.HeaderText = "Descripcion";
+            this.descripcion.Name = "descripcion";
+            this.descripcion.ReadOnly = true;
+            this.descripcion.Width = 350;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox2.Controls.Add(this.lblEstado);
+            this.groupBox2.Controls.Add(this.cboPerfil);
+            this.groupBox2.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.groupBox2.Location = new System.Drawing.Point(138, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(548, 90);
+            this.groupBox2.TabIndex = 11;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Seleccione un perfil";
+            // 
+            // lblEstado
+            // 
+            this.lblEstado.AutoSize = true;
+            this.lblEstado.BackColor = System.Drawing.Color.Transparent;
+            this.lblEstado.Font = new System.Drawing.Font("Segoe UI", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEstado.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblEstado.Location = new System.Drawing.Point(35, 41);
+            this.lblEstado.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblEstado.Name = "lblEstado";
+            this.lblEstado.Size = new System.Drawing.Size(66, 25);
+            this.lblEstado.TabIndex = 23;
+            this.lblEstado.Text = "Perfil:";
+            // 
+            // cboPerfil
+            // 
+            this.cboPerfil.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cboPerfil.FormattingEnabled = true;
+            this.cboPerfil.Location = new System.Drawing.Point(109, 38);
+            this.cboPerfil.Name = "cboPerfil";
+            this.cboPerfil.Size = new System.Drawing.Size(367, 28);
+            this.cboPerfil.TabIndex = 22;
+            // 
+            // frmConsltarPermisos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(819, 599);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBoxConsulta);
+            this.Controls.Add(this.panelBotones);
+            this.Name = "frmConsltarPermisos";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Consultar Permisos ";
+            this.Load += new System.EventHandler(this.frmConsltarPermisos_Load);
+            this.panelBotones.ResumeLayout(false);
+            this.groupBoxConsulta.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvConsultaUsuarios)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelBotones;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.GroupBox groupBoxConsulta;
+        private System.Windows.Forms.DataGridView dgvConsultaUsuarios;
+        private System.Windows.Forms.DataGridViewTextBoxColumn numForm;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn descripcion;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label lblEstado;
+        private System.Windows.Forms.ComboBox cboPerfil;
+    }
+}
